c95e7efd3b9f02b921734143143474c0567fa42a
Juan Gaines <outs.juandavid.gaines@huawei.com>
2021-05-05 17:53:17 -0500

    TODO commit

--- app/src/main/java/com/exampledtse/universitypushkit/MainActivity.kt
+++ app/src/main/java/com/exampledtse/universitypushkit/MainActivity.kt
@@ -5,11 +5,7 @@ import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
 import android.os.Bundle
-import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-import com.huawei.agconnect.config.AGConnectServicesConfig
-import com.huawei.hms.aaid.HmsInstanceId
-import com.huawei.hms.common.ApiException
 import kotlinx.android.synthetic.main.activity_main.*
 
 
@@ -48,27 +44,7 @@ class MainActivity : AppCompatActivity() {
 
         //TODO (4): Obtener token a traves de  HmsInstanceId ->getInstance->getToken
         //Se debe obtener appId y token scope (HCM)
-        object : Thread() {
-            override fun run() {
-                try {
 
-                    val appId = AGConnectServicesConfig.fromContext(this@MainActivity)
-                        .getString("client/app_id")
-                    val tokenScope = "HCM"
-                    val token =
-                        HmsInstanceId.getInstance(this@MainActivity).getToken(appId, tokenScope)
-                    Log.i(TAG, "El token es: $token")
-                    //En caso de que este vacio el token, como alternativa se
-                    // puede obtener por el metodo onNewToken() en el servicio que se extiende de HmsMessageService
-
-                    //Una vez obtenido el token se puede hacer el registro en el backend para almacenarlo
-                    // y posteriormente utilizarlo
-
-                } catch (e: ApiException) {
-                    Log.e(TAG, "get token failed, $e")
-                }
-            }
-        }.start()
     }
 
     inner class RideBroadcastReceiver : BroadcastReceiver() {
